f2fa31d499a10c282b71d050fff13387f3095d56
Johan McGregor <47840123+JnMcgregor1999@users.noreply.github.com>
2022-06-11 00:31:57 -0600

    Initial setting

---
+++ Presentation/Nop.Web/Themes/ShopAll/Content/scripts/ShopAll.js
@@ -0,0 +1,43 @@
+﻿(function ($, ssEx) {
+
+    window.themeSettings = {
+        themeBreakpoint: 1024,
+        isAccordionMenu: false
+    };
+
+    $(document).ready(function () {
+        var responsiveAppSettings = {
+            isEnabled: true,
+            themeBreakpoint: window.themeSettings.themeBreakpoint,
+            isSearchBoxDetachable: true,
+            doesDesktopHeaderMenuStick: false,
+            doesScrollAfterFiltration: true,
+            doesSublistHasIndent: true,
+            displayGoToTop: true,
+            hasStickyNav: true,
+            selectors: {
+                menuTitle: ".menu-title",
+                headerMenu: ".header-menu",
+                closeMenu: ".close-menu",
+                movedElements: ".admin-header-links, .header, .responsive-nav-wrapper, .slider-wrapper, .master-wrapper-content, .footer",
+                sublist: ".header-menu .sublist",
+                overlayOffCanvas: ".overlayOffCanvas",
+                withSubcategories: ".with-subcategories",
+                filtersContainer: ".nopAjaxFilters7Spikes",
+                filtersOpener: ".filters-button span",
+                searchBoxOpener: ".search-wrap > span",
+                searchBox: ".search-box.store-search-box",
+                searchBoxBefore: ".header-links-wrapper",
+                navWrapper: ".responsive-nav-wrapper",
+                navWrapperParent: ".responsive-nav-wrapper-parent",
+                headerLinksOpener: "#header-links-opener",
+                shoppingCartLink: ".shopping-cart-link"
+            }
+        };
+
+        ssEx.initResponsiveTheme(responsiveAppSettings);
+
+        // CUSTOM SELECTS
+        $(".product-selectors select").simpleSelect();
+    });
+})(jQuery, sevenSpikesEx);
